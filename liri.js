ea6e1448fae4117a9c899773749015ef5fdad830
Brady Douthit <brady.douthit99@gmail.com>
2019-07-24 12:19:53 -0400

    Updated the concert-this command so it works with multiple parameters

--- liri.js
+++ liri.js
@@ -6,29 +6,34 @@ const moment = require('moment');
 var spotify = new Spotify(keys.spotify);
 var searchParam = "";
 var nodeArgs = process.argv;
-
 var command = process.argv[2];
 
-function searchSong() {
+
+//Makes sure that searches work even when there are multiple words and spaces
+var paramCheck = function () {
   for (var i = 3; i < nodeArgs.length; i++) {
     if (i > 3 && i < nodeArgs.length) {
       searchParam = searchParam + " " + nodeArgs[i];
     }
     else {
       searchParam += nodeArgs[i];
-  
+
     }
   }
+}
+//spotify-this-song
+function searchSong() {
+  paramCheck();
   if (searchParam === "") {
     searchParam = "The Sign " + "Ace of Base";
     console.log("You must specify a song, but heres The Sign by Ace of Base.")
   };
-  
+
   spotify
     .search({ type: 'track', query: searchParam })
 
     .then(function (response) {
-      
+
       var song = response.tracks.items[0].name;
       var link = response.tracks.items[0].external_urls.spotify;
       var album = response.tracks.items[0].album.name;
@@ -46,11 +51,13 @@ function searchSong() {
       console.log(err);
     });
 };
-
+//concert-this
 function bandsInTown() {
+  paramCheck();
   axios.get("https://rest.bandsintown.com/artists/" + searchParam + "/events?app_id=codingbootcamp")
     .then(response => {
       console.log("\n--------------------\n")
+      console.log(searchParam.charAt(0).toUpperCase() + searchParam.slice(1) + ":\n")
       console.log(response.data[0].venue.name);
       console.log(response.data[0].venue.city + ', ' + response.data[0].venue.region);
       var timeFormatted = new moment(response.data[0].datetime).format("MM/DD/YYYY");
