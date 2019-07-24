3620ae7664420c4d26d7c67e12fa123c50c194be
Brady Douthit <brady.douthit99@gmail.com>
2019-07-24 11:59:45 -0400

    Spotify-this-song now defaults to The Sign when a song is not specified

--- liri.js
+++ liri.js
@@ -2,27 +2,43 @@ require("dotenv").config();
 const keys = require("./keys.js");
 const Spotify = require('node-spotify-api');
 const axios = require('axios');
-const moment = require('moment'); 
+const moment = require('moment');
 var spotify = new Spotify(keys.spotify);
-var searchParam = process.argv[3];
-if (process.argv[4] != undefined) {
-  searchParam = process.argv[3] + ' ' + process.argv[4];
-}
+var searchParam = "";
+var nodeArgs = process.argv;
+
 var command = process.argv[2];
 
 function searchSong() {
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      searchParam = searchParam + " " + nodeArgs[i];
+    }
+    else {
+      searchParam += nodeArgs[i];
+  
+    }
+  }
+  if (searchParam === "") {
+    searchParam = "The Sign " + "Ace of Base";
+    console.log("You must specify a song, but heres The Sign by Ace of Base.")
+  };
+  
   spotify
     .search({ type: 'track', query: searchParam })
+
     .then(function (response) {
+      
       var song = response.tracks.items[0].name;
       var link = response.tracks.items[0].external_urls.spotify;
       var album = response.tracks.items[0].album.name;
       var artist = response.tracks.items[0].artists[0].name;
+
       console.log("\n--------------------\n")
       console.log("Artist(s): " + artist)
       console.log("Album: " + album);
       console.log("Song: " + song);
-      console.log("Link" + link);
+      console.log("Link: " + link);
       console.log("\n--------------------\n")
 
     })
